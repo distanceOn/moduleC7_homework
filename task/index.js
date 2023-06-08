42861fa25defd457e9436bb907e6ca5ff3e7292b
distanceOn <distanceOn@yandex.ru>
2023-06-08 23:26:08 +0300

    solve

---
+++ task/index.js
@@ -0,0 +1,10 @@
+function revertString(str) {
+  let revertedStr = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    revertedStr += str[i];
+  }
+  return revertedStr;
+}
+
+export default revertString;
